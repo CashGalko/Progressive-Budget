73fb6d6a0d3f846ef4badf3130382813e269b6d4
CGalko <cash.galko12@ncf.edu>
2021-05-18 20:26:37 -0400

    Added onsuccess and onerror functions to db.js to handle backend processing.

--- public/db.js
+++ public/db.js
@@ -17,3 +17,23 @@ request.onupgradeneeded = function (e) {
       db.createObjectStore('BudgetStore', { autoIncrement: true });
     }
   };
+
+//   Handles the request error.
+  request.onerror = function (e) {
+    console.log(`Woops! ${e.target.errorCode}`);
+  };
+
+//   Handles the request success
+request.onsuccess = function (e) {
+    console.log('success');
+    db = e.target.result;
+  
+    // If the app is online, it will fire the checkDB function
+    if (navigator.onLine) {
+      console.log('Backend online!');
+      checkDB();
+    }
+  };
+
+//   Checks to see if app comes online, then fires the checkDB function to update the backend with any changes.
+window.addEventListener('online', checkDB);
